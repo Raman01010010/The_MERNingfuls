1f2d8777cda3a45e826000faa95a41c264d9c707
Vivek9744 <priyanshusingh202010@gmail.com>
2023-03-25 02:04:16 +0530

    chat ui and update in login

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -2,39 +2,38 @@ import React from 'react'
 import "../App.css";
 
 export default function Login() {
+  const [user,setUser]=React.useState({})
+  function handleChange(event){
+    setUser(old=>{
+      return({
+        ...old,
+        [event.target.name]:event.target.value
+      }
+      )
+    })
+  
+
+  }
   return (
       <div className='login-page'>
       <form>
   {/* Email input */}
   <div className=''></div>
   <div className="form-outline mb-4" >
-    <input type="email" id="form2Example1" className="form-control" placeholder='Email address' />
+    <input onChange={handleChange} type="email" id="form2Example1" className="form-control" placeholder='Email address' />
     <label className="form-label" htmlFor="form2Example1">
     </label>
   </div>
   {/* Password input */}
   <div className="form-outline mb-4">
-    <input type="password" id="form2Example2" className="form-control" placeholder='Password'/>
+    <input onChange={handleChange} on type="password" id="form2Example2" className="form-control" placeholder='Password'/>
     <label className="form-label" htmlFor="form2Example2">
     </label>
   </div>
   {/* 2 column grid layout for inline styling */}
   <div className="row mb-4">
     <div className="col d-flex justify-content-center">
-      {/* Checkbox */}
-      <div className="form-check">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          defaultValue=""
-          id="form2Example31"
-          defaultChecked=""
-        />
-        <label className="form-check-label" htmlFor="form2Example31">
-          {" "}
-          Remember me{" "}
-        </label>
-      </div>
+      
     </div>
     <div className="col">
       {/* Simple link */}
