141cd8f1be3bcb3fa5aa594a0d89be05b815a1fe
Raman01010010 <rmnprjrrr@gmail.com>
2023-03-25 02:16:06 +0530

    api for register user  added

---
+++ client/src/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+const URL="http://localhost:8003"
+
+//const URL="https://a1-w3yj.onrender.com"
+export const addUser=async (data)=>{
+    try{
+        return await axios.post(`${URL}/register`,data)
+
+    }catch(error){
+        console.log("Error add",error.response.data)
+        return error
+    }
+}
