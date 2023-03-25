0e3244d977d2f53ac135d9da750900a9665a3d14
Raman01010010 <rmnprjrrr@gmail.com>
2023-03-26 00:17:18 +0530

    ui updates

--- client/src/comp/SideBar.js
+++ client/src/comp/SideBar.js
@@ -448,7 +448,7 @@ export default function SideBar(props) {
       <footer className="footer">
         <div className="d-sm-flex justify-content-center justify-content-sm-between">
           <span className="text-muted d-block text-center text-sm-left d-sm-inline-block">
-            Copyright © College Connect 2023
+            Copyright © Nervo Care 2023
           </span>
           <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
             {" "}
--- client/src/comp/SidebarH.js
+++ client/src/comp/SidebarH.js
@@ -371,7 +371,7 @@ export default function SideBar(props) {
       <footer className="footer">
         <div className="d-sm-flex justify-content-center justify-content-sm-between">
           <span className="text-muted d-block text-center text-sm-left d-sm-inline-block">
-            Copyright © College Connect
+            Copyright © NerVo Care
           </span>
        
         </div>
